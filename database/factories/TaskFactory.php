c46a12f85b6715aeff9771969b8033738ecee1ee
doobie-droid <lesliedouglas23@gmail.com>
2025-03-28 14:07:37 +0100

    feat: create task factory and add to db seeder

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'project_id' => Project::factory()->create(),
+            'priority' => random_int(1, 100),
+        ];
+    }
+}
