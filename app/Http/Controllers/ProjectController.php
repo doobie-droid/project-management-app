36b9ebdc94617abfbc6a52d553f75651dd47d776
doobie-droid <lesliedouglas23@gmail.com>
2025-03-28 15:25:25 +0100

    feat: create index page for listing projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,7 +15,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::paginate(10);
+        return view('projects.index', compact('projects'));
     }
 
     /**
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -8,9 +8,40 @@
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Project Index Page
-    </h1>
+    <div class="flex justify-center min-h-screen">
+        <div class="w-full sm:w-[80%] bg-gray-200 p-2 flex flex-col items-center ">
+            <h1 class="text-2xl font-bold mb-4">Project List</h1>
+            @if ($projects->isEmpty())
+                <p>No projects found</p>
+            @else
+                <table class="min-w-full bg-white border border-gray-200 rounded-lg">
+                    <thead>
+                        <tr class=" text-left">
+                            <th class="py-2 px-4 border-b">ID</th>
+                            <th class="py-2 px-4 border-b">Name</th>
+                            <th class="py-2 px-4 border-b">Price</th>
+                            <th class="py-2 px-4 border-b">Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <td class="py-2 px-4 border-b">{{ $project->id }}</td>
+                                <td class="py-2 px-4 border-b">{{ $project->name }}</td>
+                                <td class="py-2 px-4 border-b">{{ $project->description }}</td>
+                                <td class="py-2 px-4 border-b">{{ $project->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+            <div class="mt-4 flex gap-2">
+                {{ $projects->links() }}
+            </div>
+        </div>
+
+    </div>
+
 </body>
 
 </html>
