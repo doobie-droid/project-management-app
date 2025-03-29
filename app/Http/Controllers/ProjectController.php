4421ecd3b57bfbf51d52de0101e298b0faeca9f3
doobie-droid <lesliedouglas23@gmail.com>
2025-03-29 14:37:00 +0100

    refactor: delete unused methods in Project cONTROLLER

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -20,16 +20,6 @@ class ProjectController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreProjectRequest  $request
@@ -42,16 +32,6 @@ class ProjectController extends Controller
         return redirect()->route('projects.index')->with('success', 'Project created successfully!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Project $project)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
